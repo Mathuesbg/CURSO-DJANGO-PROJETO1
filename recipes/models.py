6fa665883f975120c4ec3f4707b6377d623b1bfc
Matheus BG <matheus.brgsz@gmail.com>
2023-12-17 21:40:27 -0300

    a

--- recipes/models.py
+++ recipes/models.py
@@ -19,12 +19,14 @@ class Recipe(models.Model):
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-    is_plublished = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
-from recipes.utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(
         request,
         template_name='recipes/pages/home.html',
-        context={'recipes': [make_recipe() for _ in range(10)], }
+        context={"recipes": recipes}
+    )
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        is_published=True,
+        category__id=category_id
+    ).order_by('-id')
+    return render(
+        request,
+        template_name='recipes/pages/category.html',
+        context={"recipes": recipes}
     )
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.get(id=id)
     return render(
         request,
         template_name='recipes/pages/recipe-view.html',
-        context={'recipe': make_recipe(),
+        context={'recipe': recipe,
                  'is_detail_page': True}
     )
