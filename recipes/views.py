efaf4d4d50d3d0e8c636d764e3813d5af556cb4b
Matheus BG <matheus.brgsz@gmail.com>
2023-12-21 14:20:32 -0300

    new category page created

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Recipe
 
 
@@ -14,19 +14,25 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        is_published=True,
-        category__id=category_id
-    ).order_by('-id')
-    return render(
-        request,
-        template_name='recipes/pages/category.html',
-        context={"recipes": recipes}
+
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True,
+            category__id=category_id
+        ).order_by('-id')
     )
+
+    return render(request, template_name='recipes/pages/category.html',
+                  context={
+                      "recipes": recipes,
+                      "title": f"Category - {recipes[0].category.name}",
+                  })
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
+
+    recipe = get_object_or_404(Recipe, is_published=True, id=id)
+
     return render(
         request,
         template_name='recipes/pages/recipe-view.html',
