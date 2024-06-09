b4edde5ffdae2b68c13bba604ba415c4df0b069f
Matheus BG <matheus.brgsz@gmail.com>
2024-06-09 16:02:34 -0300

    classes on testing started

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_recipe_home_URL_is_correct(self):
+        url = reverse("recipes:home")
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_home_URL_is_correct(self):
+        url = reverse("recipes:category", args=(1,))
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_page_URL_is_correct(self):
+        url = reverse("recipes:recipe", args=(3,))
+        self.assertEqual(url, '/recipes/3/')
